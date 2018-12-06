a91adee22590a067472f386e1644924205c6e9c8
Timothy Tan <timtan93@gmail.com>
2018-12-06 15:42:17 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
-def alphabetize(arr)
-  # code here
+
+ORDER = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(array)
+  array.sort_by { |words| words.split("").map{ |letters| ORDER.index(letters) } }
 end
